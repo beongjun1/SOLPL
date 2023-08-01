4752f7cc786f449d1a391e959d6159fab50cf5b0
JungYeon <jyp75580@gmail.com>
2023-08-01 22:17:36 +0900

    nowpost 댓글 화면 윗부분

---
+++ app/src/main/java/com/example/solpl1/mainPost/NowPostCommentActivity.java
@@ -0,0 +1,16 @@
+package com.example.solpl1.mainPost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.solpl1.R;
+
+public class NowPostCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_post_comment);
+    }
+}
